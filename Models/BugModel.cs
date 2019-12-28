a9f4003e6825349eb1dd4332c3352cee971a18c6
radiantwanderer <radiantgamewanderer@gmail.com>
2019-12-28 11:06:41 -0600

    Started adding fields to BugModel

---
+++ Models/BugModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace NagaBugTracking.Models
+{
+    public class BugModel
+    {
+        public int Id { get; set; }
+
+        private string summary;
+        private string description;
+
+        [DataType(DataType.Date)]
+        private DateTime dateFound;
+
+        [DataType(DataType.Date)]
+        private DateTime lastEdited;
+    }
+}
