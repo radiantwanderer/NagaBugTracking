d8a33a76f53f310689ab9283a992e36ca8c3dbfb
radiantwanderer <radiantgamewanderer@gmail.com>
2019-12-31 10:19:02 -0600

    Specified all fields for BugModel

--- Models/BugModel.cs
+++ Models/BugModel.cs
@@ -10,8 +10,57 @@ namespace NagaBugTracking.Models
     {
         public int Id { get; set; }
 
+        public enum BugType 
+        { 
+            Defect = 0,
+            Enhancement = 1,
+            Task = 2
+        }
+
+        public enum Severity
+        {
+            Blocker = 0,
+            Critical = 1,
+            Normal = 2,
+            Minor = 3
+        }
+
+        public enum Status
+        {
+            Unconfirmed = 0,
+            New = 1,
+            Assigned = 2,
+            Resolved = 3,
+            Verified = 4,
+            Reopen = 5,
+            Closed = 6
+        }
+
+        public enum Platform
+        {
+            Unspecified = 0,
+            Windows = 1,
+            Mac = 2,
+            Linux = 3,
+            All = 4
+        }
+
+        private bool open;
+
+        private float version;
+        private int priority;
+
         private string summary;
         private string description;
+        private string product;
+        private string component;
+        private string reporter;
+        private string assignee;
+
+        private BugType bugType;
+        private Severity severity;
+        private Status status;
+        private Platform platform;
 
         [DataType(DataType.Date)]
         private DateTime dateFound;
